55f2e420a7ab7d911a2a655d99451a688a8bf7df
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-07-18 16:16:02 -0300

    Requisito 13

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 
 export const Context = createContext();
@@ -11,7 +12,7 @@ function Provider({ children }) {
 
   const [optionValue, setOptionValue] = useState('');
 
-  const [response, setResponse] = useState([]);
+  const [response, setResponse] = useState(null);
 
   const [search, setSearch] = useState('');
 
@@ -33,6 +34,19 @@ function Provider({ children }) {
     response,
     setResponse,
   };
+  const { push, location: { pathname } } = useHistory();
+
+  useEffect(() => {
+    if (pathname === '/foods' && response !== null && response.meals.length === 1) {
+      const idMeals = response.meals[0].idMeal;
+      push(`/foods/${idMeals}`);
+    }
+
+    if (pathname === '/drinks' && response !== null && response.drinks.length === 1) {
+      const idDrinks = response.drinks[0].idDrink;
+      push(`/drinks/${idDrinks}`);
+    }
+  }, [push, pathname, response]);
 
   return (
     <Context.Provider value={ contextValue }>
