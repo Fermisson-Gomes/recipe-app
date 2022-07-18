520cbc2d492e6db0fd2108df79f447fb80db3539
Patricia Eliseu <patyeliseu4@gmail.com>
2022-07-18 17:08:37 -0300

    inicio req 14

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import RecipeCard from '../components/RecipeCard';
 import Footer from '../components/Footer';
 
 function Foods() {
   return (
     <div className="foods">
       <Header />
+      <RecipeCard />
       <Footer />
     </div>
   );
