e8a5320e961a08dd8252835a90de52f300036223
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-07-18 18:52:19 -0300

    Finaliza requisito 14

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -4,7 +4,7 @@ import { Context } from '../context/Context';
 
 function RecipeCard() {
   const { response } = useContext(Context);
-  // const twelve = 12;
+  const twelve = 12;
   // const array = response.slice(0, twelve);
   const history = useHistory();
   const {
@@ -13,36 +13,38 @@ function RecipeCard() {
 
   return (
     <div>
-      {pathname.endsWith('foods') && response && response.meals.map((e, index) => (
-        <div
-          key={ index }
-          data-testid={ `${index}-recipe-card` }
-        >
-          <p>
-            { e.strMeal }
-          </p>
-          <img
-            src={ e.strMealThumb }
-            data-testid={ `${index}-card-img` }
-            alt={ e.strMeal }
-          />
-        </div>
-      ))}
-      {pathname.endsWith('drinks') && response && response.drinks.map((e, index) => (
-        <div
-          key={ index }
-          data-testid={ `${index}-recipe-card` }
-        >
-          <p>
-            { e.strDrink }
-          </p>
-          <img
-            src={ e.strDrinkThumb }
-            data-testid={ `${index}-card-img` }
-            alt={ e.strDrink }
-          />
-        </div>
-      ))}
+      {pathname.endsWith('foods') && response && response.meals.slice(0, twelve)
+        .map((e, index) => (
+          <div
+            key={ index }
+            data-testid={ `${index}-recipe-card` }
+          >
+            <p data-testid={ `${index}-card-name` }>
+              { e.strMeal }
+            </p>
+            <img
+              src={ e.strMealThumb }
+              data-testid={ `${index}-card-img` }
+              alt={ e.strMeal }
+            />
+          </div>
+        ))}
+      {pathname.endsWith('drinks') && response && response.drinks.slice(0, twelve)
+        .map((e, index) => (
+          <div
+            key={ index }
+            data-testid={ `${index}-recipe-card` }
+          >
+            <p data-testid={ `${index}-card-name` }>
+              { e.strDrink }
+            </p>
+            <img
+              src={ e.strDrinkThumb }
+              data-testid={ `${index}-card-img` }
+              alt={ e.strDrink }
+            />
+          </div>
+        ))}
     </div>
   );
 }
