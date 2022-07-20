ae556f7ae56e9b87246e04043e5b3831c4f6e1b6
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-07-20 20:15:45 -0300

    Corrige lint

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -10,7 +10,8 @@ function RecipeDetails(props) {
     location: { pathname },
   } = history;
   const { match: { params: { id } } } = props;
-  const { setDetail, details } = useContext(Context);
+  const { setDetail, details, setIngredient, ingredient,
+    setMeasure, measure } = useContext(Context);
 
   useEffect(() => {
     const Details = async () => {
@@ -30,6 +31,19 @@ function RecipeDetails(props) {
     history.push(`${id}/in-progress`);
   };
 
+  useEffect(() => {
+    if (details) {
+      const ingredients = Object.entries(details?.meals[0])
+        .filter((item) => item[0].includes('strIngredient'));
+      const secondElementIng = ingredients.map((item) => item[1]);
+      setIngredient(secondElementIng);
+      const measures = Object.entries(details?.meals[0])
+        .filter((item) => item[0].includes('strMeasure'));
+      const secondElementMea = measures.map((item) => item[1]);
+      setMeasure(secondElementMea);
+    }
+  }, [details, setIngredient, setMeasure]);
+
   return (
     <>
       {pathname.includes('foods')
@@ -47,14 +61,28 @@ function RecipeDetails(props) {
         <p data-testid="instructions">
           { details?.meals[0].strInstructions }
         </p>
-        <button
-          data-testid="start-recipe-btn"
-          className="start-recipe-btn"
-          type="button"
-          onClick={ handleClick }
-        >
-          Start Recipe
-        </button>
+        <object width="425" height="350">
+          <param name="movie" value={ details?.meals[0].strYoutube } />
+          <embed
+            src={ details?.meals[0].strYoutube }
+            type="video/mp4"
+            data-testid="video"
+            width="425"
+            height="350"
+          />
+        </object>
+        <ul>
+          {measure && measure.map((item, index) => (
+            <li data-testid={ `${index}-ingredient-name-and-measure` } key={ index }>
+              { item }
+            </li>
+          ))}
+          {ingredient && ingredient.map((item, index) => (
+            <li data-testid={ `${index}-ingredient-name-and-measure` } key={ item }>
+              { item }
+            </li>
+          ))}
+        </ul>
       </div>)}
       {pathname.includes('drinks')
     && (
@@ -79,6 +107,7 @@ function RecipeDetails(props) {
         >
           Start Recipe
         </button>
+
       </div>)}
     </>
   );
