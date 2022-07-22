c9cf5984547612f8ca8232f5905faaab2e68eb8a
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-07-22 07:39:22 -0300

    Corrige 35 e 36

--- src/components/RecipesDetailsIcons.js
+++ src/components/RecipesDetailsIcons.js
@@ -32,7 +32,7 @@ function RecipesDetailsIcons() {
         setIsFavorite(true);
       }
     }
-  }, []);
+  }, [pathname, setIsFavorite, details]);
 
   const handleClickShare = () => {
     const url = `http://localhost:3000${pathname}`;
