81a7a2ea8c592ddda08093aa610cd90d1311ff64
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-07-25 13:23:42 -0300

    Requisito 29

--- src/components/ButtonStartRecipe.js
+++ src/components/ButtonStartRecipe.js
@@ -1,37 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 
 function ButtonStartRecipe(props) {
   const history = useHistory();
   // const [btnText, setBtnText] = useState('');
-  // const [isBtnVisible, setIsBtnVisible] = useState(true);
+  const [isBtnVisible, setIsBtnVisible] = useState(true);
   const {
     location: { pathname },
   } = history;
   const { id } = props;
 
-  // useEffect(() => {
-  //   const getDoneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-  //   const getInProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes'));
-  //   const mapgetDoneRecipes = getDoneRecipes.some((item) => item.id === id);
-  //   const keysInProgressDrinks = Object.keys(getInProgressRecipes.cocktails);
-  //   const keysInProgressFoods = Object.keys(getInProgressRecipes.meals);
-  //   const mapKeysInProgressDrinks = keysInProgressDrinks.some((item) => item === id);
-  //   const mapKeysInProgressFoods = keysInProgressFoods.some((item) => item === id);
+  useEffect(() => {
+    // localStorage.setItem('doneRecipes', JSON.stringify([{
+    //   id: '52771',
+    //   type: 'food',
+    //   nationality: 'Italian',
+    //   category: 'Vegetarian',
+    //   alcoholicOrNot: '',
+    //   name: 'Spicy Arrabiata Penne',
+    //   image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+    //   doneDate: '22/6/2020',
+    //   tags: ['Pasta', 'Curry'],
+    // },
+    // {
+    //   id: '178319',
+    //   type: 'drink',
+    //   nationality: '',
+    //   category: 'Cocktail',
+    //   alcoholicOrNot: 'Alcoholic',
+    //   name: 'Aquamarine',
+    //   image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+    //   doneDate: '23/6/2020',
+    //   tags: [],
+    // },
+    // ]));
+    // localStorage.setItem('inProgressRecipes', JSON.stringify({
+    //   cocktails: {
+    //     78319: [],
+    //   },
+    //   meals: {
+    //     52771: [],
+    //   },
+    // }));
+    const getDoneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+    // const getInProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes'));
 
-  //   if (mapgetDoneRecipes) {
-  //     setIsBtnVisible(false);
-  //   } else if (!getInProgressRecipes) {
-  //     setBtnText('Continue Recipe');
-  //   } else {
-  //     setBtnText('Start Recipe');
-  //   }
-  // });
+    if (getDoneRecipes) {
+      const mapgetDoneRecipes = getDoneRecipes.some((item) => item.id === id);
+      setIsBtnVisible(!mapgetDoneRecipes);
+    }
+    // if (getInProgressRecipes) {
+    //   const keysInProgressDrinks = Object.keys(getInProgressRecipes.cocktails);
+    //   const keysInProgressFoods = Object.keys(getInProgressRecipes.meals);
+    //   const mapKeysInProgressDrinks = keysInProgressDrinks.some((item) => item === id);
+    //   const mapKeysInProgressFoods = keysInProgressFoods.some((item) => item === id);
+    //   if (mapKeysInProgressDrinks || mapKeysInProgressFoods) {
+    //     setBtnText('Continue recipe');
+    //   } else {
+    //     setBtnText('Start recipe');
+    //   }
+    // }
+  }, [id]);
 
   return (
     <>
-      {pathname.includes('foods')
+      {pathname.includes('foods') && isBtnVisible
     && (
       <button
         data-testid="start-recipe-btn"
@@ -43,7 +77,7 @@ function ButtonStartRecipe(props) {
         Start Recipe
       </button>
     )}
-      {pathname.includes('drinks')
+      {pathname.includes('drinks') && isBtnVisible
     && (
       <button
         data-testid="start-recipe-btn"
